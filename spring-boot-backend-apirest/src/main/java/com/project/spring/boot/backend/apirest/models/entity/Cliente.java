e9c9fb3b10e6e6ac9ffc862c554d8457221041ed
ERICKORTIZRODRIGUEZ <ortizerick166@gmail.com>
2021-05-30 22:33:34 -0600

    backend creacion de entidades factura,facturaItems,productos

--- spring-boot-backend-apirest/src/main/java/com/project/spring/boot/backend/apirest/models/entity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/project/spring/boot/backend/apirest/models/entity/Cliente.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name="clientes")
@@ -53,6 +55,12 @@ public class Cliente implements Serializable{
 //    public void prePersist(){
 //        createAt = new Date();
 //    }
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "cliente",cascade = CascadeType.ALL)
+    private List<Factura> facturas;
+
+    public Cliente() {
+        this.facturas = new ArrayList<>();
+    }
 
     public long getId() {
         return id;
@@ -110,5 +118,13 @@ public class Cliente implements Serializable{
 
     public void setRegion(Region region) {
         this.region = region;
+    }
+
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
+
+    public void setFacturas(List<Factura> facturas) {
+        this.facturas = facturas;
     }
 }
