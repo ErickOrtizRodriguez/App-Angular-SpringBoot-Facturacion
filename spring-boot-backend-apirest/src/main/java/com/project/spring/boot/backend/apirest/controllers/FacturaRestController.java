dec3d443e065471696d95cbf547aeb6b18d71bc3
ERICKORTIZRODRIGUEZ <ortizerick166@gmail.com>
2021-06-02 23:39:16 -0600

    implementado SprigSecurity backen y frontend

--- spring-boot-backend-apirest/src/main/java/com/project/spring/boot/backend/apirest/controllers/FacturaRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/project/spring/boot/backend/apirest/controllers/FacturaRestController.java
@@ -6,6 +6,7 @@ import com.project.spring.boot.backend.apirest.models.entity.Producto;
 import com.project.spring.boot.backend.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,24 +18,28 @@ public class FacturaRestController {
     @Autowired
     private IClienteService clienteService;
 
+    @Secured({"ROLE_USER","ROLE_ADMIN"})
     @GetMapping("/facturas/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Factura VerFactura(@PathVariable Long id){
         return clienteService.findFacturasById(id);
     }
 
+    @Secured({"ROLE_ADMIN"})
     @DeleteMapping("/facturas/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteFactura(@PathVariable Long id){
         clienteService.deleteFacturaById(id);
     }
 
+    @Secured({"ROLE_ADMIN"})
     @GetMapping("/facturas/filtrar-productos/{term}")
     @ResponseStatus(HttpStatus.OK)
     public List<Producto> filtrarProductos(@PathVariable String term){
         return clienteService.findProductoByNombre(term);
     }
 
+    @Secured({"ROLE_ADMIN"})
     @PostMapping("/facturas")
     @ResponseStatus(HttpStatus.CREATED)
     public Factura crear(@RequestBody Factura factura){
